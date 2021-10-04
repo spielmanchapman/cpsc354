6753ec88b903aab6aabb53fdc08aa6594e9a5f24
Corey Spielman <71470809+spielmanchapman@users.noreply.github.com>
2021-10-04 00:26:35 -0700

    Added new operations including subtraction + sqrt

--- Assignment1/Part1/arithmetic.hs
+++ Assignment1/Part1/arithmetic.hs
@@ -38,19 +38,36 @@ multN :: NN -> NN -> NN
 multN O m = O
 multN (S n) m = addN (multN n m) m
 
+-- subtract natural numbers
+subN :: NN -> NN -> NN
+subN O m = O
+subN m O = m
+subN (S m) (S n) = subN m n
+
 ----------------
 -- II Arithmetic
 ----------------
 
---addI :: II -> II -> II
---addI O m = m
---addI (S n) m = S (addI n m)
+-- Addition:
+addI :: II -> II -> II
+addI (II a b) (II c d) = II (addN a c) (addN b d)
+
+-- Multiplication:
+multI :: II -> II -> II
+multI (II a b) (II c d) = II (multN a c) (multN b d)
+
+-- Subtraction:
+subtrI :: II -> II -> II
+subtrI (II a b) (II c d) = II (subN a d) (subN b c)
+
+-- Negation:
+negI :: II -> II
+negI (II a b) = (II b a)
 
 ----------------
 -- QQ Arithmetic
 ----------------
 
--- add + numbers
 addP :: PP -> PP -> PP
 addP I m = (T m)
 addP (T n) m =  addP n (T m)
@@ -59,12 +76,47 @@ addP (T n) m =  addP n (T m)
 -- Normalisation
 ----------------
 
+normalizeI :: II -> II
+normalizeI (II n O) = (II n O)
+normalizeI (II O m) = (II O m)
+normalizeI (II (S n) (S m)) = normalizeI (II n m)
 
 ----------------------------------------------------
 -- Converting between VM-numbers and Haskell-numbers
 ----------------------------------------------------
 
+-- Precondition: Inputs are non-negative
+nn_int :: Integer -> NN
+nn_int 0 = O
+nn_int n = addN (S O) (nn_int (n - 1))
 
+int_nn :: NN -> Integer
+int_nn O = 0
+int_nn (S n) = 1 + (int_nn n)
+
+ii_int :: Integer -> II
+ii_int 0 = II O O
+ii_int n = II (nn_int(n)) O
+
+int_ii :: II -> Integer
+int_ii (II O O) = 0
+int_ii (II (n) m) = int_nn(n) - int_nn(m)
+
+-- Precondition: Inputs are positive
+pp_int :: Integer -> PP
+pp_int 1 = I
+pp_int n = addP I (pp_int (n - 1))
+
+int_pp :: PP -> Integer
+int_pp I = 1
+int_pp (T n) = 1 + int_pp (n)
+
+------------------------------
+-- Normalisation by Evaluation
+------------------------------
+
+nbv :: II -> II
+nbv m = ii_int (int_ii m)
 ----------
 -- Testing
 ----------
