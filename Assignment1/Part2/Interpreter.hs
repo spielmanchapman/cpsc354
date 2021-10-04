1c638b35c8343b001d6822e88caf202a59195498
CCow <44686625+CCow@users.noreply.github.com>
2021-10-03 23:36:00 -0700

    Added 6 operations to the calculator
    We decided to add Division, Subtraction, Mod, exponent (raised), Square Root, and Negative to the calculator operators.

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -6,3 +6,11 @@ eval :: Exp -> Integer
 eval (Num n) = n
 eval (Plus n m) = (eval n) + (eval m)
 eval (Times n m) = (eval n) * (eval m)
+eval (Div n m) = (eval n) / (eval m)
+eval (Sub n m) = (eval n) - (eval m)
+eval (Mod n m) = (eval n) % (eval m)
+eval (Raised n m) = (eval n) ^ (eval m)
+eval (Sqrt n) = float(sqrt(eval n))
+eval (Neg n) = (-1) * (eval n)
+
+eval (addP n m) = (eval n) + (eval m)
