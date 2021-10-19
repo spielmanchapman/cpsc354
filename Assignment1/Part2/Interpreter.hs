b2ac47d0bcc1a4fe6d95103192c2f951757b9996
CCow <44686625+CCow@users.noreply.github.com>
2021-10-18 22:03:24 -0700

    Add files via upload
    Changed mod, /, and % to different operations with '`' and compiled on my MacBook

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -6,9 +6,9 @@ eval :: Exp -> Integer
 eval (Num n) = n
 eval (Plus n m) = (eval n) + (eval m)
 eval (Times n m) = (eval n) * (eval m)
-eval (Div n m) = (eval n) / (eval m)
+eval (Div n m) = (eval n) `div` (eval m)
 eval (Sub n m) = (eval n) - (eval m)
-eval (Mod n m) = (eval n) 'mod' (eval m)
+eval (Mod n m) = (eval n) `mod` (eval m)
 eval (Raised n m) = (eval n) ^ (eval m)
 eval (Sqrt n) = floor(sqrt(fromIntegral(eval n)))
 eval (Neg n) = (-1) * (eval n)
