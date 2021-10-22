35a3203fed88197e4db096b733346c7395260180
CCow <44686625+CCow@users.noreply.github.com>
2021-10-22 14:02:37 -0700

    Add files via upload
    Changed numbers.cf to better represent order of operations, and in interpreter; we changed `div` to quot at the advice of Dan Haub in the SI session on Friday

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -5,10 +5,10 @@ import AbsNumbers
 eval :: Exp -> Integer
 eval (Num n) = n
 eval (Plus n m) = (eval n) + (eval m)
-eval (Times n m) = (eval n) * (eval m)
-eval (Div n m) = (eval n) `div` (eval m)
 eval (Sub n m) = (eval n) - (eval m)
+eval (Times n m) = (eval n) * (eval m)
+eval (Div n m) = quot (eval n) (eval m)
 eval (Mod n m) = (eval n) `mod` (eval m)
 eval (Raised n m) = (eval n) ^ (eval m)
 eval (Sqrt n) = floor(sqrt(fromIntegral(eval n)))
-eval (Neg n) = (-1) * (eval n)
+eval (Neg n) = (eval n) * (-1)
